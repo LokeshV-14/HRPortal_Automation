cbd1524d365c9ef0e860214b138a7078d5e0508c
mukunthvj <mukunthvijay.v@syncfusion.com>
2025-03-03 01:48:35 +0530

    Added Achievements helper file

---
+++ tests/Achievements.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect, Page } from '@playwright/test';
+import{Login_user,AchievementsModuleNavigation,ProgressTab,BadgesTab,StatisticsTab,HistoryTab}from './Achievements.helper';
+test.describe('Test case : Test run for Performance module', async () => {
+    test('Verify the Overview module and its sub tabs', async ({ page }) => {
+        await Login_user(page, 'vijays@syncfusion.com', 'staging');
+        await AchievementsModuleNavigation(page);
+        await ProgressTab(page);
+        await BadgesTab(page);
+        await StatisticsTab(page);
+        await HistoryTab(page);   
+    })
+})
