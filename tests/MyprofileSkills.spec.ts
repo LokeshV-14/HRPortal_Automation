8621146a2faaf821bf788e7280b08eb3d2d53b5b
mukunthvj <mukunthvijay.v@syncfusion.com>
2025-03-20 09:18:51 +0530

    Added spec file for the skills

--- tests/MyprofileSkills.spec.ts
+++ tests/MyprofileSkills.spec.ts
@@ -1,9 +1,41 @@
-import { test, expect, Page } from '@playwright/test';
-import{Login_user,SkillsModuleNavigation,LearningPortalFunctionality}from './MyProfileSkills.helper';
+import { test, expect,BrowserContext, Page } from '@playwright/test';
+import{Login_user,SkillsModuleNavigation,LearningPortalFunctionality,WebinarSubTab,CoursesAndCertificationsTab}from './MyProfileSkills.helper';
 test.describe('Test case : Test run for Skills module', async () => {
-    test('Verify the Skills module and its sub tabs', async ({ page }) => {
+   
+
+let context: BrowserContext;
+let page: Page;
+
+test.describe('Test case: Skills Module Flow', () => {
+    
+    test.beforeAll(async ({ browser }) => {
+        context = await browser.newContext();
+        page = await context.newPage();
+
+        // Call login function directly
         await Login_user(page, 'vijays@syncfusion.com', 'staging');
+    });
+
+    test('Verify the Skills Module Navigation User', async () => {
         await SkillsModuleNavigation(page);
+    });
+
+    test('Verify the Learning Portal Functionality', async () => {
         await LearningPortalFunctionality(page);
-    })
+    });
+
+    test('Verify the Webinar Sub Tab', async () => {
+        await WebinarSubTab(page);
+    });
+
+    test('Verify the Courses And Certifications Tab', async () => {
+        await CoursesAndCertificationsTab(page);
+    });
+
+    test.afterAll(async () => {
+        await context.close();
+    });
+
+});
+
 })
